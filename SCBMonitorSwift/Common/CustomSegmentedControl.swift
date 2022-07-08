33549e6fd058d7cf2f078dffee7fbb73c1bfc811
Murtazin <renatmurtazin@yahoo.com>
2022-07-08 11:44:53 +0300

    some fixes in segmented control

---
+++ SCBMonitorSwift/Common/CustomSegmentedControl.swift
@@ -0,0 +1,8 @@
+//
+//  CustomSegmentedControl.swift
+//  SCBMonitorSwift
+//
+//  Created by Renat Murtazin on 08.07.2022.
+//
+
+import Foundation
--- SCBMonitorSwift/Models/Device.swift
+++ SCBMonitorSwift/Models/Device.swift
@@ -1,9 +1,14 @@
 import Foundation
 import UIKit
 
+enum OperatingSystem {
+    case iOS
+    case android
+}
+
 struct Device {
     var id: Int
-    var operatingSystem: String
+    var operatingSystem: OperatingSystem
     var image: UIImage?
     var title: String
     var subtitle: String
